15a34994869e2b8690eea49fcc05e9dfd3452597
池田 <sephiroth929@gmail.com>
2018-05-20 15:59:37 +0900

    fix(space) RootDrawer

--- navigator/RootDrawer.js
+++ navigator/RootDrawer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
-import { createDrawerNavigator} from 'react-navigation';
+import { createDrawerNavigator } from 'react-navigation';
 
 const HomeScreen = () => (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
